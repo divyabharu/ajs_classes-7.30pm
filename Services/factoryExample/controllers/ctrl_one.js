00799e060277390f7ac721f876fcef2ec2a92284
NareshIT <hr@nareshit.in>
2017-07-10 20:43:00 +0530

    factory !!!

---
+++ Services/factoryExample/controllers/ctrl_one.js
@@ -0,0 +1,4 @@
+app.controller("ctrl_one",ctrl_one);
+function ctrl_one($scope,my_service) {
+    $scope.var_one = my_service.key1;
+}
---
+++ Services/factoryExample/js/my_class.js
@@ -0,0 +1,9 @@
+function my_class(arg1,arg2) {
+    this.fun_one = function () {
+      return arg1;
+    };
+
+    this.fun_two = function () {
+        return arg2;
+    };
+}
