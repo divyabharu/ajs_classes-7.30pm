f9160422447d36f028377e335252d96215e532df
Samba <samba.pendela@gmail.com>
2017-07-24 06:45:54 +0530

    Predefined Directives

---
+++ Directives/ctrl.js
@@ -0,0 +1,8 @@
+app.controller("ctrl",ctrl);
+function ctrl($scope) {
+    $scope.array = [{'s_id':1,'s_name':'Neeraja'},
+        {'s_id':2,'s_name':'kundana'},
+        {'s_id':3,'s_name':'Naresh'},
+        {'s_id':4,'s_name':'Balaji'},
+        {'s_id':5,'s_name':'Teja'}];
+}
