21b0d59e555028df0b947b3569650197c40836f4
Naresh <hr@nareshit.in>
2017-07-26 11:10:42 +0530

    Angular Applications

---
+++ AngularApplications/DynamicAngularApplication/controllers/ctrl.js
@@ -0,0 +1,10 @@
+/*
+app.controller("ctrl", function ($scope) {
+    $scope.var_one = "Data From DataBase Soon....";
+});*/
+
+
+app.controller("ctrl", ctrl);
+function ctrl($scope){
+    $scope.var_one = "Data From DataBase Soon...";
+}
