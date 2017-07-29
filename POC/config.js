7608376456ed90468d7adf233b3be87efbfb4fb6
Naresh <hr@nareshit.in>
2017-07-29 20:47:46 +0530

    Final Class !!!

--- POC/config.js
+++ POC/config.js
@@ -1,4 +1,20 @@
-app.config(config);
+/*
+    $stateChangeStart is a interceptor in ui.router module
+    this interceptor will execute before each routing in application
+    in general interceptors will implement by using $rootScope
+ */
+
+app.run(run).config(config);
+function run($localStorage,$rootScope,$location) {
+    $rootScope.$on("$stateChangeStart",function (event,fromState,fromParams,toState,toParams) {
+
+        if(!$localStorage.poc){
+            $location.path("/login");
+        }
+    });
+};
+
+
 function config($stateProvider,$urlRouterProvider) {
     $urlRouterProvider.otherwise("/login");
     $stateProvider.state("login",{
