68bc069643a487dc1341aff9fde7afd16d67302e
Naresh <hr@nareshit.in>
2017-07-31 11:14:59 +0530

    Final Class !!!

---
+++ Directives/ctrl.js
@@ -0,0 +1,11 @@
+app.controller("ctrl",ctrl);
+function ctrl($scope) {
+    $scope.array = [
+        {'id':1,'name':'AngularJS'},
+        {'id':2,'name':'Angular2'},
+        {'id':3,'name':'reactJS'},
+        {'id':4,'name':'NodeJS'},
+        {'id':5,'name':'mongodb'}
+    ];
+
+}
