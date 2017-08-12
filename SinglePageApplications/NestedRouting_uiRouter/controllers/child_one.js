ae9294a13aa9f46d3de265f3e709666ac0df3966
NareshIT <hr@nareshit.in>
2017-08-12 08:35:36 +0530

    Single Page Applications_2

---
+++ SinglePageApplications/NestedRouting_uiRouter/controllers/child_one.js
@@ -0,0 +1,4 @@
+app.controller("child_one",child_one);
+function child_one($scope) {
+    $scope.child_one = "I am from child_one controller !";
+}
---
+++ SinglePageApplications/NestedRouting_uiRouter/controllers/child_two.js
@@ -0,0 +1,4 @@
+app.controller("child_two",child_two);
+function child_two($scope) {
+    $scope.child_two = "I am from child_two controller !";
+}
